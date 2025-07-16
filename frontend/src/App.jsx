caed74ee49b6f54f8b4779df0529efc2db38c16a
Shaon CMD <shaoncmd@gmail.com>
2025-07-16 19:17:00 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
+import UserLogin from "./pages/UserLogin.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
